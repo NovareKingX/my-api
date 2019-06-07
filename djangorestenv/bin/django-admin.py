1fc3b3b2d6ccc4f337a3e24008d313ad49d36c96
NovareKingX <ricardo.calura@novare.com.hk>
2019-06-07 14:14:45 +0800

    Remove virtualenv

--- djangorestenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/king/Documents/Novare/Training_1/DjangoREST/djangorestenv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
